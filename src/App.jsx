701ea2d90fae82788b7b18d89c39d204ffa530e4
yeonmi park <yeonmitc@gmail.com>
2025-04-18 21:42:13 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import AppLayout from '@/layout/AppLayout'
 import NotFound from '@/pages/NotFound'
 import MoviePage from '@/pages/Movies/MoviePage'
 import { MovieDetailPage } from './pages/MovieDetail/MovieDetailPage'
-import {HomePage} from './pages/Homepage/HomePage'
+import HomePage from './pages/Homepage/HomePage'
 
 // 홈페이지  / 
 // 영화 전체보여주는 페이지 (서치)  /movies?q=ssdfa
 // 영화 디테일 페이지 /movies/:id 
 // 추천영화 /movies/:id/recommandation
 // 리뷰 /movies/:id/reviews
-//test
+
 export default function App() {
   return (
     <>
